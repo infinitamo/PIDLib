b90217e0e26e34ef5d349e684f9d62b61c17ada7
infinitamo <104581728+infinitamo@users.noreply.github.com>
2024-11-22 20:22:48 -0600

    Code Refactoring
    1. changed floats to doubles
    2. created PIDLib namespace to wrap the code
    3. made run function more modular
    4. created begin and reset functions

--- Visual_Studio/PIDLib/PIDLib/main.cpp
+++ Visual_Studio/PIDLib/PIDLib/main.cpp
@@ -2,19 +2,19 @@
 #include <iostream>
 #include "PIDLib.hpp"
 
-PIDLib_MeasurementType myMeas{};
-PIDLib_PIDSetupType myPIDSetup{};
+PIDLib::MeasurementType myMeas{};
+PIDLib::SetupType myPIDSetup{};
 
-PIDLib_PID myPID{};
+PIDLib::PID myPID{};
 
 int main()
 {
     // PID Setup 
-    float gainP = 1.0f;
-    float gainI = 0.1f;
-    float gainD = 0.5f;
-    float outputLowerLimit = -10.0f;
-    float outputUpperLimit = +10.0f;
+    double gainP = 1.0;
+    double gainI = 0.1;
+    double gainD = 0.5;
+    double outputLowerLimit = -10.0;
+    double outputUpperLimit = +10.0;
 
     myPIDSetup.gainP = gainP;
     myPIDSetup.gainI = gainI;
@@ -23,21 +23,21 @@ int main()
     myPIDSetup.outputUpperLimit = outputUpperLimit;
 
     // PID Init
-    myPID.init(myPIDSetup);
+    myPID.begin(myPIDSetup);
 
     // PID Run
-    float setpoint = 5.0f;
+    double setpoint = 5.0;
 
-    float output{};
+    double output{};
 
     myPID.setpoint(setpoint);
     
     myMeas.time_usec = 1000;
-    myMeas.value     = 3.14f;
+    myMeas.value     = 3.14;
     output = myPID.run(myMeas); // test 1st pass
 
     myMeas.time_usec = 2000;
-    myMeas.value     = 3.5f;
+    myMeas.value     = 3.5;
     output = myPID.run(myMeas); // test 2nd pass
 
     return 0;
