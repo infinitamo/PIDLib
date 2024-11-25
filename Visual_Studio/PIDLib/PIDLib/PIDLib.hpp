51352e512508bb26f217fa4fa8b3204f83dd554c
infinitamo <104581728+infinitamo@users.noreply.github.com>
2024-11-24 21:41:55 -0600

    Derivative NaN protection
    Added a step in the derivative computation to check against a very small time delta to ensure NaNs are avoided from dividing by a near-zero number

--- Visual_Studio/PIDLib/PIDLib/PIDLib.hpp
+++ Visual_Studio/PIDLib/PIDLib/PIDLib.hpp
@@ -5,13 +5,13 @@
 //  Copyright(c) 2024 infinitamo
 //  
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this softwareand associated documentation files(the "Software"), to deal
+//  of this software and associated documentation files(the "Software"), to deal
 //  in the Software without restriction, including without limitation the rights
 //  to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
 //  copies of the Software, and to permit persons to whom the Software is
 //  furnished to do so, subject to the following conditions :
 //  
-//  The above copyright noticeand this permission notice shall be included in all
+//  The above copyright notice and this permission notice shall be included in all
 //  copies or substantial portions of the Software.
 //  
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
@@ -46,11 +46,11 @@ namespace PIDLib
         constexpr double USEC_2_SEC = 1.0e-6;
     }
 
-    // Error Mode Enumeration
+    // PIDonX Enumeration
     enum ONX_MODE
     {
-        ERROR = 0,
-        MEASR = 1
+        ERROR = 0,  // X = E (for error)
+        MEASR = 1   // X = M (for measurement)
     };
 
     //============================================================
